62cfdff160fd954a67469f3e4d9f5dcef320f8d0
RockNoobgoob <64040710+RockNoobgoob@users.noreply.github.com>
2026-03-02 00:13:21 -0500

    Delete ReSpeak/ReSpeak directory

--- ReSpeak/ReSpeak/Repositories/RepositoryContainer.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// Central locator for data repository instances.
-final class RepositoryContainer {
-    static let shared = RepositoryContainer()
-    private init() {}
-}
--- ReSpeak/ReSpeak/Services/ServiceContainer.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// Central service locator for application-wide services.
-final class ServiceContainer {
-    static let shared = ServiceContainer()
-    private init() {}
-}
